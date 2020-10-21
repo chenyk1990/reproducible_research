7e2579d8cfefdabb0426ea516b154640fe4ef608
Yangkang Chen <chenyk1990@gmail.com>
2020-10-21 15:33:22 -0500

    up

--- diffrlrra/matfun/fxydmssa_win.m
+++ diffrlrra/matfun/fxydmssa_win.m
@@ -1,5 +1,5 @@
 function [ D1 ] = fxydmssa_win(D,flow,fhigh,dt,N,NN,verb,n1win,n2win,n3win,r1,r2,r3)
-%  FXYDMSSA_WIN: local DRR method
+%  FXY_MSSA: F-XY domain multichannel singular spectrum analysis (MSSA)
 %
 %  IN   D:   	intput 3D data
 %       flow:   processing frequency range (lower)
@@ -24,16 +24,9 @@ function [ D1 ] = fxydmssa_win(D,flow,fhigh,dt,N,NN,verb,n1win,n2win,n3win,r1,r2
 %  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %  GNU General Public License for more details: http://www.gnu.org/licenses/
 %
-%  REFERENCES
-%  Wang et al., 2020, Separation and imaging of seismic diffractions using a localized rank-reduction method with adaptively selected ranks, doi: 10.1190/geo2020-0215.1.
-%  Chen et al., 2019, Obtaining free USArray data by multi-dimensional seismic reconstruction, Nature Communications, 10:4434.
-%  Bai et al., 2020, Seismic signal enhancement based on the lowrank methods, Geophysical Prospecting, 68, 2783-2807.
-%  Chen et al., 2020, Five-dimensional seismic data reconstruction using the optimally damped rank-reduction method, Geophysical Journal International, 222, 1824-1845.
-%  Chen, Y., W. Huang, D. Zhang, W. Chen, 2016, An open-source matlab code package for improved rank-reduction 3D seismic data denoising and reconstruction, Computers & Geosciences, 95, 59-66.
-%  Chen, Y., D. Zhang, Z. Jin, X. Chen, S. Zu, W. Huang, and S. Gan, 2016, Simultaneous denoising and reconstruction of 5D seismic data via damped rank-reduction method, Geophysical Journal International, 206, 1695-1717.
-%  Huang, W., R. Wang, Y. Chen, H. Li, and S. Gan, 2016, Damped multichannel singular spectrum analysis for 3D random noise attenuation, Geophysics, 81, V261-V270.
-%  Chen et al., 2017, Preserving the discontinuities in least-squares reverse time migration of simultaneous-source data, Geophysics, 82, S185-S196.
-% 
+%  Reference:   Simultaneous seismic data denoising and reconstruction via multichannel
+%               singular spectrum analysis, Geophysics, 2011, 76, V25-V32
+%
 % DEMO: test/test_win3d_fxymssa.m
 
 if nargin==0
