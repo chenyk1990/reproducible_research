679a081a79e17286b2384bab2089596692d7bd01
Yangkang Chen <chenyk1990@gmail.com>
2022-06-25 00:41:14 -0500

    up

--- diffrlrra/matfun/fxydmssa_win.m
+++ diffrlrra/matfun/fxydmssa_win.m
@@ -24,9 +24,16 @@ function [ D1 ] = fxydmssa_win(D,flow,fhigh,dt,N,NN,verb,n1win,n2win,n3win,r1,r2
 %  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %  GNU General Public License for more details: http://www.gnu.org/licenses/
 %
-%  Reference:   Simultaneous seismic data denoising and reconstruction via multichannel
-%               singular spectrum analysis, Geophysics, 2011, 76, V25-V32
-%
+%  REFERENCES
+%  Wang et al., 2020, Separation and imaging of seismic diffractions using a localized rank-reduction method with adaptively selected ranks, doi: 10.1190/geo2020-0215.1.
+%  Chen et al., 2019, Obtaining free USArray data by multi-dimensional seismic reconstruction, Nature Communications, 10:4434.
+%  Bai et al., 2020, Seismic signal enhancement based on the lowrank methods, Geophysical Prospecting, 68, 2783-2807.
+%  Chen et al., 2020, Five-dimensional seismic data reconstruction using the optimally damped rank-reduction method, Geophysical Journal International, 222, 1824-1845.
+%  Chen, Y., W. Huang, D. Zhang, W. Chen, 2016, An open-source matlab code package for improved rank-reduction 3D seismic data denoising and reconstruction, Computers & Geosciences, 95, 59-66.
+%  Chen, Y., D. Zhang, Z. Jin, X. Chen, S. Zu, W. Huang, and S. Gan, 2016, Simultaneous denoising and reconstruction of 5D seismic data via damped rank-reduction method, Geophysical Journal International, 206, 1695-1717.
+%  Huang, W., R. Wang, Y. Chen, H. Li, and S. Gan, 2016, Damped multichannel singular spectrum analysis for 3D random noise attenuation, Geophysics, 81, V261-V270.
+%  Chen et al., 2017, Preserving the discontinuities in least-squares reverse time migration of simultaneous-source data, Geophysics, 82, S185-S196.
+% 
 % DEMO: test/test_win3d_fxymssa.m
 
 if nargin==0
